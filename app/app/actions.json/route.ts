e4f71965fa2f481ab94492ee9a6415d8b2dc0ab5
Sabb <sarafaabbas@gmail.com>
2024-09-12 02:51:23 +0100

    let's see now

--- app/app/actions.json/route.ts
+++ app/app/actions.json/route.ts
@@ -6,13 +6,13 @@ export const GET = async () => {
       // map all root level routes to an action
       {
         pathPattern: "/**",
-        apiPath: "/api/action",
+        apiPath: "/api/action/**",
       },
       // idempotent rule as the fallback
-      {
-        pathPattern: "/",
-        apiPath: "/api/action",
-      },
+      // {
+      //   pathPattern: "/",
+      //   apiPath: "/api/action",
+      // },
     ],
   };
 
